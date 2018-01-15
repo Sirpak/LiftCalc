304956f3e470fcfc60f7c23d55cf3f34288cc901
David Czerepak <daveczerepak@gmail.com>
2018-01-14 23:45:07 -0500

    Added Calc and Style
    The table now responds to the to the Set button and drop down menu that allows you to set you goal.
    
    Styling was aslo added, but not finished.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,8 @@
 
 
-/*let max = document.querySelector("#oneRepMax").value;
 
-function setMax() {
-   	let max = document.querySelector("#oneRepMax").value;
-    document.getElementById("repOut").innerHTML = max;
 
-    let ninetyPerc = max*.90;
-    let eightyPerc = max*.80;
-    let seventyPerc = max*.70;
-    let sixtyPerc = max*.60;
-    let fiftyPerc = max*.50;
-    let fortyPerc = max*.40;
-    document.getElementById("ninetyPerc").innerHTML = ninetyPerc;
-    document.getElementById("eightyPerc").innerHTML = eightyPerc;
-    document.getElementById("seventyPerc").innerHTML = seventyPerc;
-    document.getElementById("sixtyPerc").innerHTML = sixtyPerc;
-    document.getElementById("fiftyPerc").innerHTML = fiftyPerc;
-    document.getElementById("fortyPerc").innerHTML = fortyPerc;
-}
-
-*/
+// Get and set max
 let max = document.querySelector("#oneRepMax").value;
 
 function setMax() {
@@ -43,4 +25,75 @@ function setMax() {
     document.getElementById("sixtyPerc").innerHTML = sixtyPerc;
     document.getElementById("fiftyPerc").innerHTML = fiftyPerc;
     document.getElementById("fortyPerc").innerHTML = fortyPerc;
+}
+
+//Get and set Goals
+
+let reps,
+    rest;
+
+function setStrength(){
+    document.getElementById("strengthCalc").style.display="none";
+    document.getElementById("strengthCalc").innerHTML = "Strength";
+    document.getElementById("strengthCalc").style.display="inline-block";
+    reps="1-6";
+    rest="3-5 min";
+    var writeReps = document.getElementsByClassName("reps");
+    var writeRest = document.getElementsByClassName("rest");
+    for (var c = 0; c < writeReps.length; c++) {
+        writeReps[c].innerHTML = reps;
+        writeRest[c].innerHTML = rest;
+    }
+    document.getElementById("rowhundred").style.backgroundColor = "#00ff00";
+    document.getElementById("rowninety").style.backgroundColor = "#00ff00";
+    document.getElementById("roweighty").style.backgroundColor = "#61dd61";
+    document.getElementById("rowseventy").style.backgroundColor = "#61dd61";
+    document.getElementById("rowsixty").style.backgroundColor = "#ffff4d";
+    document.getElementById("rowfifty").style.backgroundColor = "#ff3333";
+    document.getElementById("rowforty").style.backgroundColor = "#ff3333";
+
+	
+}
+
+function setMass(){
+    document.getElementById("strengthCalc").style.display="none";
+    document.getElementById("strengthCalc").innerHTML = "Mass";
+     document.getElementById("strengthCalc").style.display="inline-block";
+     reps="7-10";
+     rest="1-2 min"
+    var writeReps = document.getElementsByClassName("reps");
+    var writeRest = document.getElementsByClassName("rest");
+    for (var c = 0; c < writeReps.length; c++) {
+        writeReps[c].innerHTML = reps;
+        writeRest[c].innerHTML = rest;
+    }
+    document.getElementById("rowhundred").style.backgroundColor = "#ffff4d";
+    document.getElementById("rowninety").style.backgroundColor = "#99ff99";
+    document.getElementById("roweighty").style.backgroundColor = "#99ff99";
+    document.getElementById("rowseventy").style.backgroundColor = "#00ff00";
+    document.getElementById("rowsixty").style.backgroundColor = "#ffff4d";
+    document.getElementById("rowfifty").style.backgroundColor = "#ff3333";
+    document.getElementById("rowforty").style.backgroundColor = "#ff3333";
+}
+
+
+function setBurn(){
+    document.getElementById("strengthCalc").style.display="none";
+    document.getElementById("strengthCalc").innerHTML = "Burn Fat";
+     document.getElementById("strengthCalc").style.display="inline-block";
+     reps="10+"
+     rest="none-30s"
+    var writeReps = document.getElementsByClassName("reps");
+    var writeRest = document.getElementsByClassName("rest");
+    for (var c = 0; c < writeReps.length; c++) {
+        writeReps[c].innerHTML = reps;
+        writeRest[c].innerHTML = rest;
+    }
+    document.getElementById("rowhundred").style.backgroundColor = "#ff3333";
+    document.getElementById("rowninety").style.backgroundColor = "#ff3333";
+    document.getElementById("roweighty").style.backgroundColor = "#99ff99";
+    document.getElementById("rowseventy").style.backgroundColor = "#00ff00";
+    document.getElementById("rowsixty").style.backgroundColor = "#99ff99";
+    document.getElementById("rowfifty").style.backgroundColor = "#ffff4d";
+    document.getElementById("rowforty").style.backgroundColor = "#ff3333";
 }
